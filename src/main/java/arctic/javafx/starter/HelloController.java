bdb88d68d5739f300c8cc2033f0056393d3672bf
arctic1106 <arcticsoft@outlook.com>
2022-02-24 11:57:47 +0100

    restructure

--- src/main/java/arctic/javafx/starter/HelloController.java
+++ src/main/java/arctic/javafx/starter/HelloController.java
@@ -1,4 +1,4 @@
-package com.arcticsoft.fx.starter;
+package arctic.javafx.starter;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.arcticsoft.javafxstarter {
     requires javafx.controls;
     requires javafx.fxml;
-    opens com.arcticsoft.fx.starter to javafx.fxml;
-    exports com.arcticsoft.fx.starter;
+    opens arctic.javafx.starter to javafx.fxml;
+    exports arctic.javafx.starter;
 }
