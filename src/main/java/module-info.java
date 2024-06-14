facfc7014af9cf32124ac99717a7c5fb7de48642
arctic1106 <arcticsoft@outlook.com>
2024-06-14 05:19:01 +0200

    upgrade

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.arcticsoft.javafxstarter {
-    requires javafx.controls;
-    requires javafx.fxml;
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+    requires transitive javafx.graphics;
     opens arctic.javafx.starter to javafx.fxml;
     exports arctic.javafx.starter;
 }
