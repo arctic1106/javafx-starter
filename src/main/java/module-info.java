913aa7c165cbea73ee6adbbfe0bd55639b92616c
arctic1106 <arcticsoft@outlook.com>
2024-11-13 20:14:53 +0100

    updates.

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.arcticsoft.javafxstarter {
-    requires transitive javafx.controls;
-    requires transitive javafx.fxml;
-    requires transitive javafx.graphics;
-    opens arctic.javafx.starter to javafx.fxml;
-    exports arctic.javafx.starter;
-}
